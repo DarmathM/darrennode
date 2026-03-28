34752d4f90cbf26f722a802200ef6951d8aba411
DarmathM <darrendarmath@gmail.com>
2026-03-28 18:50:09 +0900

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post("/contact", async (req, res) => {
   try {
     await resend.emails.send({
       from: "onboarding@resend.dev",
-      to: "darrendarmath@gmail.com",
+      to: "contact@subseatronics.com",
       subject: subject || "Nouveau Message",
       html: `
         <p><b>Name:</b> ${name}</p>
